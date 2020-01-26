512072ce7efc2934090f8c1518c02a2bd6310c3d
Shai Demri <shai@vivus.ai>
2020-01-26 21:25:10 +0200

    Updates

---
+++ precache-manifest.3979efb0de1703d92699d863323dc5da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ddf719c6e5aab60accf739592ce4b1",
+    "url": "/weather-displayer/index.html"
+  },
+  {
+    "revision": "9795e28462104ec5acd4",
+    "url": "/weather-displayer/static/js/2.07e1c19c.chunk.js"
+  },
+  {
+    "revision": "0b1e8647e82c18f1bbe8b4a59fb3fbb8",
+    "url": "/weather-displayer/static/js/2.07e1c19c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3781a160969dc6d39c7a",
+    "url": "/weather-displayer/static/js/main.8ecd2529.chunk.js"
+  },
+  {
+    "revision": "c331d5c13a35334f3bc8",
+    "url": "/weather-displayer/static/js/runtime-main.00fcb1a6.js"
+  }
+]);
